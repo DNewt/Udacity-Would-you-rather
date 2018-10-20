7d77aafbcfc776595b6491d94b5c80e5e14055d5
Yuzuki-S <yuzuki.sadoh@gmail.com>
2018-10-20 14:04:15 +1300

    create react app

--- would-you-rather/src/App.js
+++ would-you-rather/src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Login from './Login.js';
-import Register from './Register.js';
-import InputFields from './InputFields.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="center">
-        <InputFields />
-        <Login />
-        <Register />
-       
-
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
